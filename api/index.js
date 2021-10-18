58de18d020ef631c06f3ba83461241775edaaa06
Andrew Peterson Account <andrew.peterson1021@gmail.com>
2021-10-17 20:44:04 -0600

    Adjusted end point logic

--- api/index.js
+++ api/index.js
@@ -8,7 +8,16 @@ class Todo {
         this.id = new Date().getTime(), 
         this.complete = false,
         this.name = name, 
-        this.category = category
+        this.category = category, 
+        this.lastModified = new Date().toISOString()
+    }
+}
+
+class Category {
+    constructor(name) {
+        this.id = new Date().getTime(),
+        this.lastModified = new Date().toISOString(),
+        this.name = name
     }
 }
 
@@ -16,15 +25,16 @@ class Todo {
 router.get('/todos', (req, res) => {
     try {
         let returnList = req.query.category ? db.toDos.filter(e => e.category === req.query.category) : db.toDos;
-        res.status(200).json(formatResponse({ status: 200, data: returnList }))
+        res.status(200).json(formatResponse({ status: 200, data: returnList }));
     } catch(error) {
-        res.status(500).json(formatResponse({ status: 500, data: error }))
+        res.status(500).json(formatResponse({ status: 500, data: error }));
     }
 })
 
 // Create todo
 router.post('/todo', (req, res) => {
     try {
+
         let newTodo = new Todo(req.body.name, req.body.category);
         db.toDos.push(newTodo);
         res.status(200).json(formatResponse({ status: 200, data: newTodo }))
@@ -41,12 +51,13 @@ router.put('/todo/:id', (req, res) => {
             db.toDos[toDoIndex].name = req.body.name;
             db.toDos[toDoIndex].category = req.body.category;
             db.toDos[toDoIndex].complete = req.body.complete;
-            res.status(200).json(formatResponse({ status: 200, data: db.toDos[toDoIndex] }))
+            db.toDos[toDoIndex].lastModified = new Date().toISOString();
+            res.status(200).json(formatResponse({ status: 200, data: db.toDos[toDoIndex] }));
         } else {
             res.status(404).json(formatResponse({ status: 404, data: `There is no item with the id of ${req.params.id} in the database`}));
         }
     } catch (error) {
-        res.status(500).json(formatResponse({ status: 500, data: error }))
+        res.status(500).json(formatResponse({ status: 500, data: error }));
     }
 })
 
@@ -56,37 +67,75 @@ router.delete('/todo/:id', (req, res) => {
         let toDoIndex = db.toDos.findIndex(e => e.id === parseInt(req.params.id));
         if (toDoIndex) {
             db.toDos.splice(toDoIndex, 1);
-            res.status(200).json(formatResponse({status: 200, data: "Item successfully deleted"}))
+            res.status(200).json(formatResponse({status: 200, data: "Item successfully deleted"}));
         } else {
             res.status(404).json(formatResponse({ status: 404, data: `There is no item with the id of ${req.params.id} in the database`}));
         }
     }catch(error) {
-        res.status(500).json(formatResponse({ status: 500, data: error }))
+        res.status(500).json(formatResponse({ status: 500, data: error }));
     }
 })
 
 // Get all categories
 router.get('/categories', (req, res) => {
     try {
-        res.status(200).json(formatResponse({ status: 200, data: db.categories }))
+        res.status(200).json(formatResponse({ status: 200, data: db.categories }));
     } catch(error) {
-        res.status(500).json(formatResponse({ status: 500, data: error }))
+        res.status(500).json(formatResponse({ status: 500, data: error }));
     }
 })
 
 // Create new category
 router.post('/category', (req, res) => {
-    
+    try {
+        const {name} = req.body;
+        const categoryExists = db.categories.find(e => e.name === name);
+        if (categoryExists) {
+            res.status(400).json(formatResponse({status: 400, data: `${name} already exists!`}));
+        } else {
+            let newCategory = new Category(name);
+            db.categories.push(newCategory);
+            res.status(200).json(formatResponse({status: 200, data: newCategory}));
+        }
+    } catch (error) {
+        res.status(500).json(formatResponse({ status: 500, data: error }));
+    }
 })
 
 // Update existing category
-router.put('/category', (req, res) => {
-    
+router.put('/category/:id', (req, res) => {
+    try {
+        let catIndex = db.categories.findIndex(e => e.id === parseInt(req.params.id));
+        if (catIndex) {
+            db.categories[catIndex].name = req.body.name;
+            db.categories[catIndex].lastModified = new Date().toISOString();
+            res.status(200).json(formatResponse({ status: 200, data: db.categories[catIndex] }));
+        } else {
+            res.status(404).json(formatResponse({ status: 404, data: `There is no category with the id of ${req.params.id} in the database`}));
+        }
+    } catch (error) {
+        res.status(500).json(formatResponse({ status: 500, data: error }))
+    }
 })
 
 // Delete category
-router.delete('/category', (req, res) => {
-    
+router.delete('/category/:id', (req, res) => {
+    try {
+        let catIndex = db.categories.findIndex(e => e.id === parseInt(req.params.id));
+        if (catIndex) {
+            db.categories.splice(catIndex, 1);
+            res.status(200).json(formatResponse({status: 200, data: "Item successfully deleted"}));
+        } else {
+            res.status(404).json(formatResponse({ status: 404, data: `There is no item with the id of ${req.params.id} in the database`}));
+        }
+    }catch(error) {
+        res.status(500).json(formatResponse({ status: 500, data: error }));
+    }
+})
+
+// I am a teapot!
+router.get('/coffee', (req, res) => {
+    res.status(418).json('Server refuses to brew coffee because it is, permanently, a teapot.');
 })
 
 // Format data for response
